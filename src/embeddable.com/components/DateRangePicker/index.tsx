be09c2805106e8d18df966cc6cc01b83636edd8d
Yash Keshri <yashrajk17@gmail.com>
2024-11-06 19:01:11 +0530

    Add date picker

--- src/embeddable.com/components/DateRangePicker/index.tsx
+++ src/embeddable.com/components/DateRangePicker/index.tsx
@@ -14,9 +14,10 @@ import Loading from "../util/Loading";
 import Error from "../util/Error";
 import ResizeListener from "../util/ResizeListener";
 import { LocalizationProvider } from "@mui/x-date-pickers";
-import { DateRangePicker } from "@mui/x-date-pickers-pro";
-import  AdapterDayjs from '@mui/x-date-pickers/AdapterDayjs/AdapterDayjs';
-import dayjs from 'dayjs';
+// import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { DatePicker } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/node/AdapterDayjs/AdapterDayjs.js";
+import dayjs from "dayjs";
 
 export type Props = {
   defaultPeriod?: TimeRange;
@@ -33,54 +34,61 @@ export default (props: Props) => {
   const [maxHeight, setMaxHeight] = useState(1000);
   const shortcutsItems = [
     {
-      label: 'This Week',
+      label: "This Week",
       getValue: () => {
         const today = dayjs();
-        return [today.startOf('week'), today.endOf('week')];
+        return [today.startOf("week"), today.endOf("week")];
       },
     },
     {
-      label: 'Last Week',
+      label: "Last Week",
       getValue: () => {
         const today = dayjs();
-        const prevWeek = today.subtract(7, 'day');
-        return [prevWeek.startOf('week'), prevWeek.endOf('week')];
+        const prevWeek = today.subtract(7, "day");
+        return [prevWeek.startOf("week"), prevWeek.endOf("week")];
       },
     },
     {
-      label: 'Last 7 Days',
+      label: "Last 7 Days",
       getValue: () => {
         const today = dayjs();
-        return [today.subtract(7, 'day'), today];
+        return [today.subtract(7, "day"), today];
       },
     },
     {
-      label: 'Current Month',
+      label: "Current Month",
       getValue: () => {
         const today = dayjs();
-        return [today.startOf('month'), today.endOf('month')];
+        return [today.startOf("month"), today.endOf("month")];
       },
     },
     {
-      label: 'Next Month',
+      label: "Next Month",
       getValue: () => {
         const today = dayjs();
-        const startOfNextMonth = today.endOf('month').add(1, 'day');
-        return [startOfNextMonth, startOfNextMonth.endOf('month')];
+        const startOfNextMonth = today.endOf("month").add(1, "day");
+        return [startOfNextMonth, startOfNextMonth.endOf("month")];
       },
     },
-    { label: 'Reset', getValue: () => [null, null] },
+    { label: "Reset", getValue: () => [null, null] },
   ];
-  
 
   return (
     <MUI>
       <ResizeListener onResize={(w, h) => setMaxHeight(h)} debounce={300}>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateRangePicker
-              defaultValue={[dayjs('2022-04-17'), dayjs('2022-04-21')]}
-            />
-      </LocalizationProvider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker/>
+          {/* <DemoContainer components={["DateRangePicker", "DateRangePicker"]}>
+            <DemoItem label="Uncontrolled picker" component="DateRangePicker">
+              <DatePicker
+                defaultValue={[dayjs("2022-04-17"), dayjs("2022-04-21")]}
+              />
+            </DemoItem>
+            <DemoItem label="Controlled picker" component="DateRangePicker">
+              <DatePicker/>
+            </DemoItem>
+          </DemoContainer> */}
+        </LocalizationProvider>
       </ResizeListener>
     </MUI>
   );
