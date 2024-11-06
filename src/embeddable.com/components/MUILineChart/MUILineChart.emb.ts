b4a0591752fc1394b9efe25d0ddd7d2dece3b7d3
rui@trevor.io <rui@embeddable.com>
2024-11-06 16:45:57 +0000

    added a area boolean to the inputs

--- src/embeddable.com/components/MUILineChart/MUILineChart.emb.ts
+++ src/embeddable.com/components/MUILineChart/MUILineChart.emb.ts
@@ -1,50 +1,56 @@
-import { EmbeddedComponentMeta, defineComponent } from '@embeddable.com/react';
+import { EmbeddedComponentMeta, defineComponent } from "@embeddable.com/react";
 
-import Component from './index';
-import { Inputs } from '@embeddable.com/react';
-import { isDimension, isMeasure, loadData } from '@embeddable.com/core';
+import Component from "./index";
+import { Inputs } from "@embeddable.com/react";
+import { isDimension, isMeasure, loadData } from "@embeddable.com/core";
 
 export const meta = {
-  name: 'MUILineChart',
-  label: 'Line Chart',
+  name: "MUILineChart",
+  label: "Line Chart",
   defaultHeight: 100,
   defaultWidth: 400,
-  category: 'Material UI',
-  classNames: ['overflow-scroll'], //defined in global.css
+  category: "Material UI",
+  classNames: ["overflow-scroll"], //defined in global.css
   inputs: [
     {
-      name: 'title',
-      type: 'string',
-      label: 'Title',
-      category: 'Settings',
+      name: "title",
+      type: "string",
+      label: "Title",
+      category: "Settings",
     },
     {
-      name: 'ds',
-      type: 'dataset',
-      label: 'Dataset to display',
-      category: 'Configure chart'
+      name: "area",
+      type: "boolean",
+      label: "Area",
+      category: "Settings",
     },
     {
-      name: 'xAxis',
-      type: 'dimension',
-      label: 'X-Axis',
+      name: "ds",
+      type: "dataset",
+      label: "Dataset to display",
+      category: "Configure chart",
+    },
+    {
+      name: "xAxis",
+      type: "dimension",
+      label: "X-Axis",
       config: {
-        dataset: 'ds',
-        supportedTypes: ['time']
+        dataset: "ds",
+        supportedTypes: ["time"],
       },
-      category: 'Configure chart'
+      category: "Configure chart",
     },
     {
-      name: 'yAxis',
-      type: 'measure',
+      name: "yAxis",
+      type: "measure",
       array: true,
-      label: 'Y-Axis',
+      label: "Y-Axis",
       config: {
-        dataset: 'ds'
+        dataset: "ds",
       },
-      category: 'Configure chart'
+      category: "Configure chart",
     },
-  ]
+  ],
 } as const satisfies EmbeddedComponentMeta;
 
 export default defineComponent(Component, meta, {
@@ -53,12 +59,14 @@ export default defineComponent(Component, meta, {
       ...inputs,
       results: loadData({
         from: inputs.ds,
-        timeDimensions: [{
-          dimension: inputs.xAxis.name,
-          granularity: 'week'
-        }],
-        measures: inputs.yAxis
-      })
+        timeDimensions: [
+          {
+            dimension: inputs.xAxis.name,
+            granularity: "week",
+          },
+        ],
+        measures: inputs.yAxis,
+      }),
     };
-  }
+  },
 });
