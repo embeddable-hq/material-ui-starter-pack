4e575168e8e7c6d00bfcc93ce8e4477b750ab867
Evgeny Oshchepkov <eugeneoshepkov@gmail.com>
2024-11-06 17:08:40 +0100

    Revert config

--- embeddable.config.ts
+++ embeddable.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from '@embeddable.com/sdk-core';
-import react from '@embeddable.com/sdk-react';
+import { defineConfig } from "@embeddable.com/sdk-core";
+import react from "@embeddable.com/sdk-react";
 
 export default defineConfig({
   plugins: [react],
@@ -7,9 +7,4 @@ export default defineConfig({
   componentsSrc: "./src/embeddable.com/components",
   errorFallbackComponent:
     "./src/embeddable.com/components/util/ErrorHandler.jsx",
-  previewBaseUrl: "https://app.dev.embeddable.com",
-  pushBaseUrl: "https://api.dev.embeddable.com",
-  audienceUrl: "https://api.dev.embeddable.com/",
-  authDomain: "embeddable-dev.eu.auth0.com",
-  authClientId: "xOKco5ztFCpWn54bJbFkAcT8mV4LLcpG",
 });
