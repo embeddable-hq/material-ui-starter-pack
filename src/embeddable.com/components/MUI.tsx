c432253c1e12bc1b3bd62616a8c6a00913cace76
Mikhail Terekhov <mikhail@embeddable.de>
2024-11-07 12:10:00 +0100

    fix formating

--- src/embeddable.com/components/MUI.tsx
+++ src/embeddable.com/components/MUI.tsx
@@ -1,21 +1,25 @@
-import createCache from "@emotion/cache";
-import { CacheProvider, EmotionCache, Theme } from "@emotion/react";
-import { ThemeProvider, createTheme } from "@mui/material";
-import React, { ReactNode, useLayoutEffect, useRef, useState } from "react";
+import createCache from '@emotion/cache';
+import { CacheProvider, EmotionCache, Theme } from '@emotion/react';
+import { ThemeProvider, createTheme } from '@mui/material';
+import React, { ReactNode, useLayoutEffect, useRef, useState } from 'react';
 
 export default (props: { children: ReactNode }) => {
   const ref = useRef<HTMLDivElement>(null);
   const [cache, setCache] = useState<EmotionCache>();
   const [theme, setTheme] = useState<Theme>();
+
   useLayoutEffect(() => {
     if (ref.current === null) return;
+
     const container = ref.current.parentElement as Element;
+
     const cache = createCache({
-      key: "css",
+      key: 'css',
       prepend: false,
       container,
     });
     setCache(cache);
+
     const theme = createTheme({
       components: {
         MuiPopover: {
@@ -35,9 +39,11 @@ export default (props: { children: ReactNode }) => {
         },
       },
     });
+
     setTheme(theme);
   }, []);
-  if (!cache || !theme) return <div ref={ref}/>;
+
+  if (!cache || !theme) return <div ref={ref} />;
   return (
     <CacheProvider value={cache}>
       <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
