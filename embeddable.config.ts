1910c2d6624e14141b70e77df08116341bb8bdc4
Yash Keshri <yashrajk17@gmail.com>
2024-11-07 16:31:23 +0530

    comment config

--- embeddable.config.ts
+++ embeddable.config.ts
@@ -7,9 +7,9 @@ export default defineConfig({
   componentsSrc: './src/embeddable.com/components',
   errorFallbackComponent: './src/embeddable.com/components/util/ErrorHandler.jsx',
 
-  previewBaseUrl: 'https://app.dev.embeddable.com',
-  pushBaseUrl: 'https://api.dev.embeddable.com/',
-  audienceUrl: 'https://api.dev.embeddable.com/',
-  authDomain: 'embeddable-dev.eu.auth0.com',
-  authClientId: 'xOKco5ztFCpWn54bJbFkAcT8mV4LLcpG'
+  // previewBaseUrl: 'https://app.dev.embeddable.com',
+  // pushBaseUrl: 'https://api.dev.embeddable.com/',
+  // audienceUrl: 'https://api.dev.embeddable.com/',
+  // authDomain: 'embeddable-dev.eu.auth0.com',
+  // authClientId: 'xOKco5ztFCpWn54bJbFkAcT8mV4LLcpG'
 });
