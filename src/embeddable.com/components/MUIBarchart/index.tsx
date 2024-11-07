312c27c77009a70feac394b1917d19bdb8860b01
Jozef Lang <jozef@embeddable.de>
2024-11-07 11:44:44 +0100

    Fixes

--- src/embeddable.com/components/MUIBarchart/index.tsx
+++ src/embeddable.com/components/MUIBarchart/index.tsx
@@ -1,9 +1,9 @@
 import {DataResponse, Dataset, Dimension, DimensionOrMeasure, Measure} from "@embeddable.com/core";
 import React from "react";
 import MUI from "../MUI";
-import Loading from "../util/Loading";
 import Error from "../util/Error";
 import {BarChart} from "@mui/x-charts";
+import {Box, CircularProgress} from "@mui/material";
 
 type Props = {
     ds: Dataset;
@@ -16,24 +16,38 @@ export default (props: Props) => {
     const { xAxis, metrics, results } = props;
     const { isLoading, data, error } = results;
 
-    if(isLoading) {
-        return <Loading />
-    }
-
     if(error) {
         return <Error msg={error}/>;
     }
 
-    const labels = data.map((d) => { return d[xAxis.name]; }) || [] as string[]
-    const series = metrics.map((m) => {
+    const labels = data?.map((d) => { return d[xAxis.name]; }) || [] as string[]
+    const series = metrics?.map((m) => {
         return {
-            data: data.map((d) => parseFloat(d[m.name])) || [],
+            data: data?.map((d) => parseFloat(d[m.name])) || [],
             label: m.title
         };
     }) || [];
 
     return (
         <MUI>
+
+            {isLoading && (
+                <Box
+                    position="absolute"
+                    top={0}
+                    left={0}
+                    right={0}
+                    bottom={0}
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    bgcolor="rgba(255, 255, 255, 0.7)"
+                    zIndex={1}
+                >
+                    <CircularProgress />
+                </Box>
+            )}
+
             <BarChart
                 xAxis={[{scaleType: 'band', data: labels }]}
                 series = {series}
