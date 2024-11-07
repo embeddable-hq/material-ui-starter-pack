caeef744bf84c88121037721c41f91000f67e8ca
Jozef Lang <jozef@embeddable.de>
2024-11-07 04:51:01 -0500

    Dropdown control & BarChart

---
+++ src/embeddable.com/components/MUIBarchart/MUIBarchart.emb.ts
@@ -0,0 +1,50 @@
+import {defineComponent, EmbeddedComponentMeta, Inputs} from "@embeddable.com/react";
+import {loadData} from "@embeddable.com/core";
+
+import Component, { Props } from './index';
+
+export const meta = {
+    name: 'MUIBarchart',
+    label: 'BarChart',
+    category: 'Material UI',
+    inputs: [
+        {
+            name: 'ds',
+            type: 'dataset',
+            label: 'Dataset',
+            description: 'Dataset'
+        },
+        {
+            name: 'xAxis',
+            type: 'dimension',
+            label: 'xAxis',
+            description: 'X-Axis',
+            config: {
+                dataset: 'ds'
+            },
+        },
+        {
+            name: 'metrics',
+            type: 'measure',
+            label: 'Metrics',
+            description: 'Metrics',
+            array: true,
+            config: {
+                dataset: 'ds'
+            },
+        },
+    ]
+} as const satisfies EmbeddedComponentMeta;
+
+export default defineComponent(Component, meta, {
+    props: (inputs: Inputs<typeof meta>) => {
+        return {
+            ...inputs,
+            results: loadData({
+                from: inputs.ds,
+                dimensions: [inputs.xAxis],
+                measures: inputs.metrics
+            })
+        };
+    }
+});
